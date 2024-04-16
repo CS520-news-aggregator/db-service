902eb46f0b5d8efee35beb32e0b9b15433b9e898
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2024-04-16 00:22:01 -0400

    fixed _id issue

--- routers/aggregator.py
+++ routers/aggregator.py
@@ -9,10 +9,12 @@ from routers.user import auth_manager, user_client
 aggregator_router = APIRouter(prefix="/aggregator")
 aggregator_client = get_mongo_client()["aggregator"]
 
+
 def change_db_id_to_str(data):
     if data:
         data["id"] = str(data["_id"])
     return data
+
 
 @aggregator_router.post("/add-aggregation")
 async def put_aggregations(post: Post = Body(...)):
@@ -22,7 +24,7 @@ async def put_aggregations(post: Post = Body(...)):
 
 
 @aggregator_router.get("/get-aggregation")
-async def get_aggregations(post_id: str):
+async def get_aggregation(post_id: str):
     post = get_post(post_id)
     return {"message": "Retrieved aggregations", "post": jsonable_encoder(post)}
 
@@ -31,7 +33,7 @@ async def get_aggregations(post_id: str):
 async def get_all_aggregations(limit: int):
     return {
         "message": "Retrieved aggregations",
-        "list_posts": list(aggregator_client["posts"].find().limit(limit)),
+        "list_posts": get_all_posts(limit),
     }
 
 
@@ -168,8 +170,10 @@ async def comment_post(comment: Comment = Body(...), user=Depends(auth_manager))
 
 @aggregator_router.get("/get-comments")
 async def get_comments(post_id: str):
-    comments = list(aggregator_client["comments"].find({"post_id": post_id}))
-    return {"message": "Retrieved comments", "comments": comments}
+    return {
+        "message": "Retrieved comments",
+        "comments": get_comments_by_post_id(post_id),
+    }
 
 
 def remove_from_attribute_list(dt_vals: dict, attribute: str, post_id: str):
@@ -199,7 +203,17 @@ def get_post(post_id: str):
     return post
 
 
+def get_all_posts(limit: int):
+    posts = list(aggregator_client["posts"].find().limit(limit))
+    return list(map(change_db_id_to_str, posts))
+
+
 def get_comment(comment_id: str):
     comment = aggregator_client["comments"].find_one({"_id": comment_id})
     comment = change_db_id_to_str(comment)
     return comment
+
+
+def get_comments_by_post_id(post_id: str):
+    comments = list(aggregator_client["comments"].find({"post_id": post_id}))
+    return list(map(change_db_id_to_str, comments))
