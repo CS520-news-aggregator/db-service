b8db368074a888ed8455f5e8cd928f8176b618a6
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2024-04-30 22:56:55 -0400

    fix

--- routers/recommender.py
+++ routers/recommender.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from models.utils.constants import RECOMMENDER_HOST
 from routers.user import auth_manager
 from models.utils.funcs import Response, get_data_from_api
+from utils import change_db_id_to_str
 
 
 recommender_router = APIRouter(prefix="/recommender")
@@ -18,4 +19,7 @@ def get_recommendations(user=Depends(auth_manager), limit: int = 5):
     ) == Response.FAILURE:
         raise HTTPException(status_code=400, detail="Could not get recommendations")
 
+    recommendations["list_recommendations"] = list(
+        map(change_db_id_to_str, recommendations["list_recommendations"])
+    )
     return recommendations
