dc628b0d2a43467b6e89e2682216ed9b1fd71359
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2024-05-11 18:48:10 -0400

    changes

--- tests/test_llm.py
+++ tests/test_llm.py
@@ -1,21 +1,24 @@
-from fastapi.testclient import TestClient
 from fastapi.encoders import jsonable_encoder
-from models.pub_sub import Subscriber
+from fastapi.testclient import TestClient
+from models.llm import PostAnalysis
 from main import app
+import mock
+from utils import get_mongo_client
 
 client = TestClient(app)
 
 
-# TODO change tests
-# def test_post_sub():
-#     sub = Subscriber(ip_address="123456789", port=1000)
-#     response = client.post("/observer/subscribe", json=jsonable_encoder(sub))
-#     assert response.status_code == 200
-#     assert response.json() == {"message": "Observer subscribed"}
-
-#     sub2 = Subscriber(ip_address="123456789", port=1000)
-#     response = client.post("/observer/subscribe", json=jsonable_encoder(sub2))
-#     assert response.status_code == 400
+@mock.patch("routers.llm.llm_client", get_mongo_client()["llm_test"])
+def test_add_analysis_no_post():
+    post_analysis = PostAnalysis(
+        post_id="random",
+        completion={
+            "title": "random",
+            "summary": "random",
+        },
+    )
+    response = client.post("/llm/add-analysis", json=jsonable_encoder(post_analysis))
+    assert response.status_code == 400
 
 
 # def test_get_sub():
